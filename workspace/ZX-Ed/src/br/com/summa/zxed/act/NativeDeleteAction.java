487801d392a06fda195b9e9fd95b47d24130e6f5
einar-saukas <einar@summa.com.br>
2019-03-14 00:26:48 -0300

    Improved error handling

--- workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeDeleteAction.java
+++ workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeDeleteAction.java
@@ -1,16 +1,23 @@
 package br.com.summa.zxed.act;
 
+import javax.persistence.*;
+
 import org.openxava.actions.*;
 
+import br.com.summa.zxed.ex.*;
 import br.com.summa.zxed.sql.*;
 
 public class NativeDeleteAction extends ViewBaseAction implements IAction {
 
     @Override
     public void execute() throws Exception {
-        NativeManager.delete(getModelName(), getView().getKeyValues(), getView().getValues());
-        resetDescriptionsCache();
-        addMessage("object_deleted", getModelName());
-        getView().clear();
+        try {
+            NativeManager.delete(getModelName(), getView().getKeyValues(), getView().getValues());
+            resetDescriptionsCache();
+            addMessage("object_deleted", getModelName());
+            getView().clear();
+        } catch (PersistenceException ex) {
+            throw new RootCauseException(ex);
+        }
     }
 }
--- workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeDeleteSelectedAction.java
+++ workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeDeleteSelectedAction.java
@@ -2,20 +2,27 @@ package br.com.summa.zxed.act;
 
 import java.util.*;
 
+import javax.persistence.*;
+
 import org.openxava.actions.*;
 import org.openxava.model.*;
 
+import br.com.summa.zxed.ex.*;
 import br.com.summa.zxed.sql.*;
 
 public class NativeDeleteSelectedAction extends TabBaseAction {
 
     @Override
     public void execute() throws Exception {
-        for (Map<String, Object> keys : getSelectedKeys()) {
-            Map<String, Object> values = MapFacade.getValues(getModelName(), keys, getView().getMembersNamesWithHidden());
-            NativeManager.delete(getModelName(), keys, values);
+        try {
+            for (Map<String, Object> keys : getSelectedKeys()) {
+                Map<String, Object> values = MapFacade.getValues(getModelName(), keys, getView().getMembersNamesWithHidden());
+                NativeManager.delete(getModelName(), keys, values);
+            }
+            getTab().deselectAll();
+            resetDescriptionsCache();
+        } catch (PersistenceException ex) {
+            throw new RootCauseException(ex);
         }
-        getTab().deselectAll();
-        resetDescriptionsCache();
     }
 }
