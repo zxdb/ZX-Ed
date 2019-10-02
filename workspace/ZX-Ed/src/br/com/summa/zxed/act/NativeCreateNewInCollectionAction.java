2319fcc8cc17bfaf8b71b99f364cb198a37201d1
einar-saukas <einar@summa.com.br>
2019-10-02 18:02:35 -0300

    Code cleaning

--- workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeCreateNewInCollectionAction.java
+++ workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeCreateNewInCollectionAction.java
@@ -1,30 +1,15 @@
 package br.com.summa.zxed.act;
 
 import org.openxava.actions.*;
-import org.openxava.view.*;
 
 public class NativeCreateNewInCollectionAction extends CreateNewElementInCollectionAction {
 
     @Override
     public void execute() throws Exception {
-        if (isParentSaved2()) {
+        if (isParentSaved()) {
             super.execute();
         } else {
             addError("zxed_parent_not_saved");
         }
-    }
-
-    protected boolean isParentSaved2() {
-        View view = getCollectionElementView().getParent();
-        if (getView() == view) {
-            if (view.isKeyEditable()) {
-                return false;
-            }
-        } else {
-            if (view.getKeyValuesWithValue().isEmpty()) {
-                return false;
-            }
-        }
-        return true;
     }
 }
--- workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeSaveAction.java
+++ workspace/ZX-Ed/src/br/com/summa/zxed/act/NativeSaveAction.java
@@ -2,12 +2,10 @@ package br.com.summa.zxed.act;
 
 import java.util.*;
 
-import javax.ejb.*;
 import javax.persistence.*;
 
 import org.openxava.actions.*;
 import org.openxava.model.*;
-import org.openxava.validators.*;
 
 import br.com.summa.zxed.ex.*;
 import br.com.summa.zxed.sql.*;
@@ -17,37 +15,14 @@ public class NativeSaveAction extends SaveAction {
     @Override
     public void execute() throws Exception {
         try {
-            if (getView().isKeyEditable()) {
-                updateView2(create2(), isResetAfterOnCreate());
-            } else {
-                updateView2(modify2(), isResetAfterOnModify());
-            }
-            resetDescriptionsCache();
-        } catch (ValidationException ex) {
-            addErrors(ex.getErrors());
-        } catch (ObjectNotFoundException ex) {
-            addError("no_modify_no_exists");
-        } catch (DuplicateKeyException ex) {
-            addError("no_create_exists");
+        	super.execute();
         } catch (PersistenceException ex) {
             throw new RootCauseException(ex);
         }
     }
 
-    protected void updateView2(Map<String, Object> values, boolean resetAfter) {
-        if (resetAfter) {
-            getView().setKeyEditable(true);
-            commit();
-            getView().reset();
-        } else {
-            getView().setKeyEditable(false);
-            if (isRefreshAfter()) {
-                getView().setValues(values);
-            }
-        }
-    }
-
-    protected Map<String, Object> modify2() throws Exception {
+    @Override
+    protected Map<String, Object> modify() throws Exception {
         Map<String, Object> keyValues = getView().getKeyValues();
         try {
             NativeManager.update(getModelName(), keyValues, getValuesToSave());
@@ -59,7 +34,8 @@ public class NativeSaveAction extends SaveAction {
         return MapFacade.getValues(getModelName(), keyValues, getView().getMembersNamesWithHidden());
     }
 
-    protected Map<String, Object> create2() throws Exception {
+    @Override
+    protected Map<String, Object> create() throws Exception {
         Map<String, Object> keyValues = getView().getKeyValues();
         NativeManager.insert(getModelName(), keyValues, getValuesToSave());
         addMessage("entity_created", getModelName());
